a3a34fda874cc3d2987cf8056d233d25f3b06751
WickedSerpent <deilrind@live.no>
2022-04-20 17:16:31 +0200

    Character Name!! :D

---
+++ Time/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string MaleMainName = "Main Protagonistson";
+            // string FemaleMainName = "Mainie Protagonistia";
+            // string male = "boy";
+            // string female = "girl";
+
+            string CharacterName = "Main Protagonist";
+            
+            Console.WriteLine("Welcome to the land of GameTitleHereia!");
+            Console.ReadKey();
+            Console.WriteLine("Tell me about yourself..");
+            Console.ReadKey();
+            Console.WriteLine("Like, whats your name? (Write your characters name)");
+            CharacterName = Console.ReadLine();
+            if (CharacterName == "")
+            {
+                Console.WriteLine(".. You there? You didn't type a name. For now we'll restart this game so you can try again");
+                CharacterName = Console.ReadLine();
+            }else{
+            Console.WriteLine("...");
+            Console.ReadLine();
+            Console.WriteLine(CharacterName+"?!" + " That's a very strange name!");
+            Console.WriteLine("You must me very special person! :D");
+            }
+
+
+        }
+    }
+}
